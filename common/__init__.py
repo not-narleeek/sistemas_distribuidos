70db40cc41630953a715e6f158d7b62f5659b855
00Hamster <K1qaUYQ9zdG@protonmail.com>
2025-10-20 20:08:37 -0300

    fix: allow generator to run locally

---
+++ common/__init__.py
@@ -0,0 +1,31 @@
+"""Utilities compartidas para la arquitectura asincrónica."""
+
+from .kafka_utils import (
+    build_consumer,
+    build_producer,
+    build_admin_client,
+    ensure_topics,
+)
+from .logging import configure_logging
+from .mongo import connect_mongo
+from .models import (
+    QuestionMessage,
+    LLMRequest,
+    LLMResponse,
+    ErrorMessage,
+    ValidatedResponse,
+)
+
+__all__ = [
+    "build_consumer",
+    "build_producer",
+    "build_admin_client",
+    "ensure_topics",
+    "configure_logging",
+    "connect_mongo",
+    "QuestionMessage",
+    "LLMRequest",
+    "LLMResponse",
+    "ErrorMessage",
+    "ValidatedResponse",
+]
